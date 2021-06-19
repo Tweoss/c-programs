be6bd5bd964ba65a67b25fc0a00dea6f1079436e
Francis Chua <francis@opennetworking.org>
2021-06-19 12:13:47 -0700

    lab 2 ending

--- labs.c
+++ labs.c
@@ -1,70 +1,13 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
+
+int main(void)
 {
-	if (argc == 2)
-	{
-		char *input = argv[1];
-		int decimal = 0;
-		int i;
-		for (i = 0; input[i] != '\0'; i++)
-		{
-			decimal = decimal << 1;
-			if (input[i] == '1')
-			{
-				// do power by shifting to the left.
-				decimal += 1;
-			}
-		}
-		printf("Decimal: %d\n", decimal);
-		printf("Hex: ");
-		// increment chunks based off the initial_offset
-		int initial_offset = i%4;
-		for (int j = initial_offset - 4; j < i; j += 4)
-		{
-			int hex_temp = 0;
-			for (int k = j; k < j + 4; k++)
-			{
-					hex_temp = hex_temp << 1;
-				if (k < 0) {
-					continue;
-				}
-				else if (input[k] == '\0') {
-					break;
-				} else if (input[k] == '1')
-				{
-					hex_temp += 1;
-				}
-			}
-			if (hex_temp < 10)
-			{
-				printf("%d", hex_temp);
-			}
-			else
-			{
-				switch (hex_temp)
-				{
-				case 10:
-					printf("A");
-					break;
-				case 11:
-					printf("B");
-					break;
-				case 12:
-					printf("C");
-					break;
-				case 13:
-					printf("D");
-					break;
-				case 14:
-					printf("E");
-					break;
-				case 15:
-					printf("F");
-					break;
-				}
-			}
-		}
-		printf("\n");
-	}
-	return 0;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            // minimizing # of printf statements. Use conditionals to determine whether character should be newline, space, or star
+            printf("%c", i + j < 4 || j - i > 4 || i + j > 12 || i - j > 4  ? j == 9 ? '\n' :' ' : '*');
+        }
+    }
 }
